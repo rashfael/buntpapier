a00f0a7262ef3be41677f03aabb8bcd1cc649596
rashfael <rashfael@isobeef.org>
2018-02-01 12:44:40 +0100

    dont console log

--- src/vuelidate/validators/index.js
+++ src/vuelidate/validators/index.js
@@ -2,7 +2,6 @@ import { withParams, req } from 'vuelidate/lib/validators/common'
 import { email as _email } from 'vuelidate/lib/validators'
 
 const required = message => withParams({message: message}, req)
-console.log(_email)
 const email = message => withParams({message: message}, _email)
 
 export {
