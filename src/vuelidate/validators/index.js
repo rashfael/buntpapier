37c04ac6a4e9122e73bee41be890c4c13fd3f9db
rashfael <rashfael@isobeef.org>
2018-04-20 13:06:45 +0200

    add minLength validator

--- src/vuelidate/validators/index.js
+++ src/vuelidate/validators/index.js
@@ -1,10 +1,12 @@
 import { withParams, req } from 'vuelidate/lib/validators/common'
-import { email as _email } from 'vuelidate/lib/validators'
+import { email as _email, minLength as _minLength } from 'vuelidate/lib/validators'
 
 const required = message => withParams({message: message}, req)
 const email = message => withParams({message: message}, _email)
+const minLength = message => withParams({message: message}, _minLength)
 
 export {
 	required,
-	email
+	email,
+	minLength
 }
