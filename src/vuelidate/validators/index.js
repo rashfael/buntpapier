e73421638bb1c09afa6e4fa13777de71afed53fa
rashfael <rashfael@isobeef.org>
2018-04-20 13:21:27 +0200

    actually accept a min length parameter

--- src/vuelidate/validators/index.js
+++ src/vuelidate/validators/index.js
@@ -1,9 +1,9 @@
-import { withParams, req } from 'vuelidate/lib/validators/common'
+import { withParams, req, len } from 'vuelidate/lib/validators/common'
 import { email as _email, minLength as _minLength } from 'vuelidate/lib/validators'
 
 const required = message => withParams({message: message}, req)
 const email = message => withParams({message: message}, _email)
-const minLength = message => withParams({message: message}, _minLength)
+const minLength = (length, message) => withParams({message: message, min: length}, value => !req(value) || len(value) >= length)
 
 export {
 	required,
