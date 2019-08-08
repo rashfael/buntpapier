491f2101684336090c9ca631ca8375405d346bf2
rashfael <rashfael@isobeef.org>
2019-08-08 16:57:12 +0200

    add maxLength validator

--- src/vuelidate/validators/index.js
+++ src/vuelidate/validators/index.js
@@ -4,9 +4,11 @@ import { email as _email } from 'vuelidate/lib/validators'
 const required = message => withParams({message: message}, req)
 const email = message => withParams({message: message}, _email)
 const minLength = (length, message) => withParams({message: message, min: length}, value => !req(value) || len(value) >= length)
+const maxLength = (length, message) => withParams({message: message, max: length}, value => !req(value) || len(value) <= length)
 
 export {
 	required,
 	email,
-	minLength
+	minLength,
+	maxLength
 }
