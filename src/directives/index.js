b94c2805b7bc5b1c4c591e16fba29e6382a32dc8
rash <sebastian.gepperth@ax-semantics.com>
2021-02-05 16:09:23 +0100

    allow for multiple resize observer directives on the same element

--- src/directives/index.js
+++ src/directives/index.js
@@ -5,13 +5,26 @@ import Tooltip from './tooltip'
 export default function (Vue) {
 	Vue.directive('resizeObserver', {
 		beforeMount (el, binding) {
-			const observer = new ResizeObserver(binding.value)
+			const observer = new ResizeObserver(entries => {
+				if (!el.__buntpapier__resize_observer) return
+				for (const handler of el.__buntpapier__resize_observer.handlers) {
+					handler(entries)
+				}
+			})
+			if (!el.__buntpapier__resize_observer) {
+				el.__buntpapier__resize_observer = {
+					observer,
+					handlers: [binding.value]
+				}
+			} else {
+				el.__buntpapier__resize_observer.handlers.push(binding.value)
+			}
 			observer.observe(el)
-			el.__buntpapier__resize_observer = observer
 		},
 		beforeUnmount (el, binding, vnode, oldVnode) {
 			if (!el.__buntpapier__resize_observer) return
-			el.__buntpapier__resize_observer.disconnect()
+			el.__buntpapier__resize_observer.observer.disconnect()
+			el.__buntpapier__resize_observer = null
 		}
 	})
 
