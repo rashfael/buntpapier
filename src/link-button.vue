c717cac7cec976cb6724a3844b2583d149715977
rash <sebastian.gepperth@ax-semantics.com>
2021-04-06 14:49:24 +0200

    apply $attrs to bunt-link-button link

--- src/link-button.vue
+++ src/link-button.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 router-link(:to="to", custom, v-slot="{ href, navigate }")
-	a.bunt-link-button(:href="href", @click="navigate($event); $emit('click', $event)")
+	a.bunt-link-button(v-bind="$attrs", :href="href", @click="navigate($event); $emit('click', $event)")
 		slot
 		ripple-ink
 </template>
